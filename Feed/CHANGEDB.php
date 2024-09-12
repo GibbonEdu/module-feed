09a4ff2c8e589a01ad2942cced64c9ffc7bed756
Ross Parker <>
2024-09-12 13:24:00 +0800

    v1.2.00

--- Feed/CHANGEDB.php
+++ Feed/CHANGEDB.php
@@ -9,41 +9,58 @@ $sql[$count][0] = '1.0.00';
 $sql[$count][1] = '-- First version, nothing to update';
 
 //v1.0.01
+++$count;
 $sql[$count][0] = '1.0.01';
 $sql[$count][1] = '';
 
 //v1.0.02
+++$count;
 $sql[$count][0] = '1.0.02';
 $sql[$count][1] = '';
 
 //v1.0.03
+++$count;
 $sql[$count][0] = '1.0.03';
 $sql[$count][1] = '';
 
 //v1.0.04
+++$count;
 $sql[$count][0] = '1.0.04';
 $sql[$count][1] = '';
 
 //v1.0.05
+++$count;
 $sql[$count][0] = '1.0.05';
 $sql[$count][1] = '';
 
 //v1.1.00
+++$count;
 $sql[$count][0] = '1.1.00';
 $sql[$count][1] = '';
 
 //v1.1.01
+++$count;
 $sql[$count][0] = '1.1.01';
 $sql[$count][1] = '';
 
 //v1.1.02
+++$count;
 $sql[$count][0] = '1.1.02';
 $sql[$count][1] = '';
 
 //v1.1.03
+++$count;
 $sql[$count][0] = '1.1.03';
 $sql[$count][1] = '';
 
 //v1.1.04
+++$count;
 $sql[$count][0] = '1.1.04';
 $sql[$count][1] = '';
+
+//v1.2.00
+++$count;
+$sql[$count][0] = '1.2.00';
+$sql[$count][1] = "
+UPDATE gibbonModule SET author='Gibbon Foundation', url='https://gibbonedu.org' WHERE name='Feed';end
+";
--- Feed/manifest.php
+++ Feed/manifest.php
@@ -1,7 +1,9 @@
 <?php
 /*
-Gibbon, Flexible & Open School System
-Copyright (C) 2010, Ross Parker
+Gibbon: the flexible, open school platform
+Founded by Ross Parker at ICHK Secondary. Built by Ross Parker, Sandra Kuipers and the Gibbon community (https://gibbonedu.org/about/)
+Copyright © 2010, Gibbon Foundation
+Gibbon™, Gibbon Education Ltd. (Hong Kong)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -25,9 +27,9 @@ $description = 'Offers a unified feed of latest posts from student and class web
 $entryURL = 'feed_view.php';
 $type = 'Additional';
 $category = 'Other';
-$version = '1.1.04';
-$author = 'Ross Parker';
-$url = 'http://rossparker.org';
+$version = '1.2.00';
+$author = "Gibbon Foundation";
+$url = "https://gibbonedu.org";
 
 //Action rows
 $actionRows[0]['name'] = 'View Feed';
